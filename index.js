17e5697fb08600b0fb50f2e45326b0873ce078b0
Kiran Patil <kirantpatil@gmail.com>
2016-07-07 13:06:19 +0530

    added app.import to index.js file

--- index.js
+++ index.js
@@ -3,4 +3,8 @@
 
 module.exports = {
   name: 'ember-leaflet-routing-machine'
+  included: function(app){
+    app.import(app.bowerDirectory + '/leaflet-routing-machine/dist/leaflet-routing-machine.js');
+    app.import(app.bowerDirectory + '/leaflet-routing-machine/dist/leaflet-routing-machine.css');
+ }
 };
